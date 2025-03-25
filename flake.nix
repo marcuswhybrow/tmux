e2cb5d1bce7ef2443a23220e0eae98f9bf5db648
Marcus Whybrow <marcus@whybrow.uk>
2025-03-25 17:34:57 +0000

    Fix conf requiring reload + Add light/dark themes
    Light theme is loaded by default
    "Ctrl + d" switches to dark theme
    "Ctrl + l" switches to light them
    
    Conf requiring a reload was due to using the -f flag, which I understand
    does not apply the conf file to new sessions only new tmux servers. By
    instead using the source-file command the conf is applied on every new
    session.
    
    Therefore I've removed the ctrl+r & ctrl+d bindings for relaoding the
    conf file in production and in development respectively; and done away
    with the whole need to link the conf file to the global XDG location.

--- flake.nix
+++ flake.nix
@@ -25,37 +25,38 @@
     } ''
       mkdir --parents $out/bin
       makeWrapper ${pkgs.tmux}/bin/tmux $out/bin/tmux \
-        --add-flags "-f $out/share/marcuswhybrow-tmux/tmux.conf"
+        --add-flags "source $out/share/marcuswhybrow-tmux/tmux.conf \;"
 
       mkdir --parents $out/share/marcuswhybrow-tmux
       ln -s ${getConf} $out/share/marcuswhybrow-tmux/get-conf
       ln -s ${linkConf} $out/share/marcuswhybrow-tmux/link-conf
 
+      cat > $out/share/marcuswhybrow-tmux/light.conf << EOF 
+      # Matches Catppuccin Late colours (but appropriate for any light theme)
+      set -g status-left-style 'fg=#8c8fa1 bg=default'
+      setw -g window-status-current-style 'fg=#4c4f69 bg=default'
+      setw -g window-status-style 'fg=#acb0be  bg=default'
+      set -g status-style fg=default,bg=default
+      set -g status-bg "default"
+      # messages
+      # set -g message-style 'fg=yellow bg=red bold'
+      # setw -g window-status-bell-style 'fg=yellow bg=red bold'
+      EOF
+
+      cat > $out/share/marcuswhybrow-tmux/dark.conf << EOF 
+      # Matches Catppuccin Mocha colours (but appropriate for any light theme)
+      set -g status-left-style 'fg=#6c7086 bg=default'
+      setw -g window-status-current-style 'fg=#CDD6F4 bg=default'
+      setw -g window-status-style 'fg=#6c7086  bg=default'
+      set -g status-style fg=default,bg=default
+      set -g status-bg "default"
+      # messages
+      # set -g message-style 'fg=yellow bg=red bold'
+      # setw -g window-status-bell-style 'fg=yellow bg=red bold'
+      EOF
+
       cat > $out/share/marcuswhybrow-tmux/tmux.conf << EOF
       run-shell ${pkgs.tmuxPlugins.catppuccin.rtp}
-
-      # Conf reloading
-
-      # Tmux sessions are long lived. Changes to tmux.conf are only checked 
-      # when a new session is created. I package my tmux conf within a nix 
-      # package, meaning there is no shared tmux conf file which is updated.
-      # Instead, each update has it's own unique conf file, inaccessible to 
-      # all other later versions.
-      #
-      # The advantage to this is that my tmux conf is portable, I can use it 
-      # in remote locations using \`nix run github:marcuswhybrow/tmux\`. The 
-      # downside is that reloading tmux's conf in a long lived session is 
-      # difficult. 
-      # 
-      # To solve this issue, I'm working out the conf file location for that 
-      # packaged version of tmux, and linking it to 
-      # ~/.config/marcuswhybrow-tmux/tmux.conf. This is bound to the \`r\` key
-      # directly before sourcing that file.
-      bind C-r run-shell "$out/share/marcuswhybrow-tmux/link-conf"\; source-file ~/.config/marcuswhybrow-tmux/tmux.conf
-
-      # During development we can do \`nix build\` to get a \`result\` directory,
-      # then manually source the conf in the result.
-      bind C-d source-file ./result/share/marcuswhybrow-tmux/tmux.conf
 
       set -g default-command "${fish}"
 
@@ -76,7 +77,6 @@
       bind j select-pane -D        # down
       bind k select-pane -U        # up
       bind l select-pane -R        # right
-
 
       # Mouse control
       set -g mouse on
@@ -104,41 +104,17 @@
 
       # Custom Theme
       set-option -g status-position top
-      # set -g status-justify left
-
-      set -g status-left-style 'fg=#8c8fa1 bg=#eff1f5'
-      set -g status-left "#S "
+      set -g status-left "󰹇 #S "
       set -g status-left-length 100
-
       set -g status-right ""
-      set -g status-right-length 50
-      set -g status-right-style 'fg=#4c4f69 bg=#dce0e8'
-
-      setw -g window-status-current-style 'fg=#4c4f69 bg=#eff1f5'
+      set -g status-right-length 0
       setw -g window-status-current-format '#I #W '
-
-      setw -g window-status-style 'fg=#acb0be  bg=#eff1f5'
       setw -g window-status-format '#I '
+      
+      source-file $out/share/marcuswhybrow-tmux/light.conf
+      bind C-l source-file $out/share/marcuswhybrow-tmux/light.conf
+      bind C-d source-file $out/share/marcuswhybrow-tmux/dark.conf
 
-      # setw -g window-status-bell-style 'fg=yellow bg=red bold'
-
-      # # messages
-      # set -g message-style 'fg=yellow bg=red bold'
-
-      set -g status-style fg=default,bg=#eff1f5
-      set -g status-bg "#eff1f5"
-
-
-      # Catppuccin Theme config
-
-      # # set -g @catppuccin_flavour 'mocha'
-      # set -g @catppuccin_flavour 'latte'
-      # set -g @catppuccin_window_current_text "#W"
-      # set -g @catppuccin_window_default_text "#W"
-
-      # # I can't get these to work??
-      # # set -g @catppuccin_status_modules_left ""
-      # set -g @catppuccin_status_modules_right "host session"
       EOF
     '';
 
